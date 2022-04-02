fb0d09a37e9a967c411792275b534341d5c37b7e
OmarHamdann <omar.haamdan@gmail.com>
2022-04-02 17:47:36 +0300

    set netPayableAmount and set discount

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -9,18 +9,27 @@ import java.util.List;
 @Service
 public class UserService {
     private List<User> usersList = new ArrayList<>(Arrays.asList(
-            new User("1", "omar", "hamdan", UserType.EMPLOYEE),
-            new User("2", "abood", "srour", UserType.EMPLOYEE),
-            new User("3", "ali", "ahmad", UserType.CUSTOMER),
-            new User("4", "salah", "ali", UserType.CUSTOMER),
-            new User("5", "moath", "mohammad", UserType.CUSTOMER)
+            new User("1", "omar", "hamdan", UserType.EMPLOYEE, 990.0),
+            new User("2", "abood", "srour", UserType.EMPLOYEE, 55.5),
+            new User("3", "ali", "ahmad", UserType.CUSTOMER, 990.0),
+            new User("4", "salah", "ali", UserType.CUSTOMER, 55.5),
+            new User("5", "moath", "mohammad", UserType.CUSTOMER, 320.0)
     ));
 
+
     public List<User> getAllUsers() {
+
+        usersList.stream().forEach(el -> el.setDiscount(el.getType()));
+        usersList.stream().forEach(el -> el.setNetPayableAmount(el.getTotal()));
+
+
         return usersList;
     }
+
     public User getUserById(String id) {
-        return usersList.stream().filter(user->user.getId().equals(id)).findFirst().get();
+        usersList.stream().forEach(el -> el.setDiscount(el.getType()));
+        usersList.stream().forEach(el -> el.setNetPayableAmount(el.getTotal()));
+        return usersList.stream().filter(user -> user.getId().equals(id)).findFirst().get();
     }
 
 }
