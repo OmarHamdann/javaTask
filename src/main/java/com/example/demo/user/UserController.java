11d5a48ee36f73040a918b007db32f916b52c1c2
OmarHamdann <omar.haamdan@gmail.com>
2022-04-03 12:47:39 +0300

    post method to create user

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,26 +1,33 @@
 package com.example.demo.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
-    private  UserService userService;
+    private UserService userService;
+//
+    @GetMapping("/users")
 
-   @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 
-    public List<User> getAllUsers(){
-       return userService.getAllUsers();
-   }
     @GetMapping("/users/{id}")
-
-    public User getUserById(@PathVariable String id){
+    public User getUserById(@PathVariable String id) {
         return userService.getUserById(id);
+    }
+
+
+    @PostMapping("/users")
+
+   public User createUser(@RequestBody User user)
+    {
+
+        return userService.createUser(user);
     }
 
 }
